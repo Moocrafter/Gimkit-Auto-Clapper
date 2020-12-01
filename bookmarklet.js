5e88449fc2e38ab03c987bc9d7a1a9a1dfad0c0a
Moocrafter <38020594+Moocrafter@users.noreply.github.com>
2020-12-01 11:05:17 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,20 +1,22 @@
 var clapping = true;
-var clapper = document.querySelector("#content DIV .sc-gyfYDX.cuxPcJ .sc-jZIEzD.eiBiTJ .sc-bRbqnn.fPQnYY DIV .sc-beKmYL.bEOmlE.fade-router-enter-done .sc-ilGAqu.frbouh.animated.zoomInDown .sc-fXchrD.jghgOw .sc-NOpvV.hFqjJJ DIV");
-var timer = setInterval(function() {clapper.click()}, 1)
+var timer;
+var clapper = document.querySelector("div.sc-NOpvV.hFqjJJ > div > div > div.animated.pulse.infinite");
 
-var btn = document.createElement("button");
-btn.id = "ToggleClap";
-btn.style = "position: absolute;top: 0;z-index: 1000;"
+const toggleClapingState = function() {
+    if (clapping) {
+        btn.innerHTML = "Start Clapping";
+        clearInterval(timer);
+    }else if (!clapping) {
+        btn.innerHTML = "Stop Clapping";
+        timer = setInterval(function() {clapper.click()}, 1)
+    }
+
+    clapping = !clapping;
+}
+
+var btn = document.createElement("BUTTON");
 btn.innerHTML = "Stop";
-btn.addEventListener("click", function() {
-    if (clapping) {
-        btn.innerHTML = "Continue";
-        clearInterval(timer);
-    }else if (!clapping) {
-        btn.innerHTML = "Stop";
-        timer = setInterval(function() {clapper.click()}, 1)
-    }
-
-    clapping = !clapping;
-});
+btn.addEventListener("click", toggleClapingState);
+//btn.id = "ToggleClap"; //For Debug Use
+btn.style = "position: absolute;top: 0;z-index: 1000;"
 document.body.appendChild(btn);
